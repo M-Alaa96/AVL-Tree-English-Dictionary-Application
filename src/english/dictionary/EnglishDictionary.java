e65e1b5325bb99c2b84714c4d422a36a513185b6
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 18:11:05 +0200

    Update EnglishDictionary.java

--- src/english/dictionary/EnglishDictionary.java
+++ src/english/dictionary/EnglishDictionary.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package english.dictionary;
 
 import java.io.BufferedReader;
@@ -14,15 +9,9 @@ import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author RS
- */
+
 public class EnglishDictionary {
 
-    /**
-     * @param args the command line arguments
-     */
     /* Creating object of AVLTree */
     static AVLTree dictionary = new AVLTree();
     static ArrayList<String> list = new ArrayList<>();
