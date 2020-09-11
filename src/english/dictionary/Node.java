198ec16352bd7881cec4fffe8b3ebfcaaa5a56b4
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 18:11:35 +0200

    Update Node.java

--- src/english/dictionary/Node.java
+++ src/english/dictionary/Node.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package english.dictionary;
 
-/**
- *
- * @author RS
- */
 public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
 
     private Node<T> leftChild, rightChild;
